c81d90ac0b16d1e2246624539effe67826f13d37
SantiagoNoguera <santi.nog23@gmail.com>
2023-05-02 23:02:56 -0500

    Se vuelve el modelo de la entidad producto para poder ser utilizado por otros servicios.

---
+++ common-producto/src/test/java/co/edu/ucentral/common/producto/commonproducto/CommonProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.common.producto.commonproducto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
