5081baa9e75a2b1dd3153fce11b703bac77cc073
SantiagoNoguera <santi.nog23@gmail.com>
2023-05-04 23:36:55 -0500

    Implementación de método para poder consultar las ventas que ha hecho un cliente en especifico, y metodo para comidicar dichas ventas.

--- servicio-ventas/src/main/java/co/edu/ucentral/servicio/ventas/controller/VentaController.java
+++ servicio-ventas/src/main/java/co/edu/ucentral/servicio/ventas/controller/VentaController.java
@@ -1,6 +1,8 @@
 package co.edu.ucentral.servicio.ventas.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,10 +10,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.edu.ucentral.servicio.ventas.model.Cliente;
 import co.edu.ucentral.servicio.ventas.model.Venta;
+import co.edu.ucentral.servicio.ventas.service.ClienteService;
 import co.edu.ucentral.servicio.ventas.service.VentaService;
 
 @RestController
@@ -19,6 +24,9 @@ public class VentaController {
 
     @Autowired
     private VentaService service;
+
+    @Autowired
+    private ClienteService serviceCliente;
 
     @GetMapping("/")
     public ResponseEntity<?> listar() {
@@ -34,6 +42,45 @@ public class VentaController {
     public ResponseEntity<?> buscarPorProductoId(@PathVariable Long id) {
         List<Venta> lista = service.findVentaByProductoId(id);
         return ResponseEntity.ok(lista);
+    }
+
+    @GetMapping("/cliente/{id}")
+    public ResponseEntity<?> buscarPorClienteId(@PathVariable Long id) {
+        Optional<Cliente> optional = serviceCliente.findById(id);
+
+        if (!optional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(optional.get());
+    }
+
+    @PutMapping("/cliente/{id}")
+    public ResponseEntity<?> modificar(@RequestBody Cliente cliente, @PathVariable Long id) {
+        Optional<Cliente> optional = serviceCliente.findById(id);
+
+        if (!optional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Cliente clienteBD = optional.get();
+        clienteBD.setNombre(cliente.getNombre());
+        clienteBD.setDireccion(cliente.getDireccion());
+        List<Venta> eliminados = new ArrayList<Venta>();
+
+        clienteBD.getVentas().forEach(venta -> {
+            if (!cliente.getVentas().contains(venta)) {
+                eliminados.add(venta);
+            }
+        });
+
+        eliminados.forEach(venta -> {
+            clienteBD.removeVenta(venta);
+        });
+
+        clienteBD.setVentas(cliente.getVentas());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(serviceCliente.save(clienteBD));
     }
     
 }
