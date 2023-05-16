1495334fa77d8f759ba9f3b7e4da7077f8b20818
SantiagoNoguera <santi.nog23@gmail.com>
2023-05-15 20:44:06 -0500

    Borrado de librarias innecesarias.

--- servicio-ventas/src/main/java/co/edu/ucentral/servicio/ventas/service/VentaService.java
+++ servicio-ventas/src/main/java/co/edu/ucentral/servicio/ventas/service/VentaService.java
@@ -1,7 +1,5 @@
 package co.edu.ucentral.servicio.ventas.service;
 
-import java.util.List;
-
 import co.edu.ucentral.common.cliente.model.Cliente;
 import co.edu.ucentral.commons.services.service.CommonService;
 import co.edu.ucentral.servicio.ventas.model.Venta;
