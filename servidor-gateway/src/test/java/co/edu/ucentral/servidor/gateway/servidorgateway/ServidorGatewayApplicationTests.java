67c7a0fa672895d486c55c43909faa0474864857
SantiagoNoguera <santi.nog23@gmail.com>
2023-06-26 22:50:44 -0500

    Creación del servidor gateway en donde se puede acceder a todos los servicios creados e inscritos a eureka desde una misma puerta de acceso.

---
+++ servidor-gateway/src/test/java/co/edu/ucentral/servidor/gateway/servidorgateway/ServidorGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.servidor.gateway.servidorgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
