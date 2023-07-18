0dbc26e53c8ef30394bed31a6e61687b281efce3
SantiagoNoguera <santi.nog23@gmail.com>
2023-07-17 22:05:14 -0500

    Se crea el servicio common de usuarios para poder referenciar el modelo en otros servicios.

---
+++ common-usuario/src/test/java/co/edu/ucentral/common/usuario/commonusuario/CommonUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.common.usuario.commonusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
