ccc795e2186f1ee221ad5462c492838404969d3d
SantiagoNoguera <santi.nog23@gmail.com>
2024-02-27 23:13:56 -0500

    Implementación de JWT para la autenticación y permisos de los usuarios. Es necesario revisar el JWT que se genera ya que es de 224 bits y no de 256 bits.

--- servicio-oauth/src/main/java/co/edu/ucentral/servicio/oauth/security/AuthorizationServerConfig.java
+++ servicio-oauth/src/main/java/co/edu/ucentral/servicio/oauth/security/AuthorizationServerConfig.java
@@ -1,8 +1,10 @@
 package co.edu.ucentral.servicio.oauth.security;
 
 import java.util.Arrays;
+import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -42,10 +44,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         .scopes("read", "write")
         .authorizedGrantTypes("password", "refresh_token")
         .accessTokenValiditySeconds(3600)
-        .refreshTokenValiditySeconds(3600);
-
+        .refreshTokenValiditySeconds(3600)
+        .and()
         //Autorización para aplicación en FronEnd hecha con Android Studio.
-        clients.inMemory().withClient("AndroidStudioApp")
+        .withClient("AndroidStudioApp")
         .secret(passwordEncoder.encode("9876543210"))
         .scopes("read", "write")
         .authorizedGrantTypes("password", "refresh_token")
@@ -70,7 +72,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     private JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey("codigo_secreto_firma_jwt");
+
+        //El Código de acceso a la aplicación se encripta en Base64, en el archivo AutorizationManagetJWT.java del servidor-gateway se hace lo mismo.
+        tokenConverter.setSigningKey(Base64.getEncoder().encodeToString("codigo_secreto_12345".getBytes()));
         return tokenConverter;
     }
 
--- servicio-usuarios/src/main/resources/import.sql
+++ servicio-usuarios/src/main/resources/import.sql
@@ -1,10 +1,10 @@
 INSERT INTO roles(nombre) VALUES ('ROLE_ADMIN');
 INSERT INTO roles(nombre) VALUES ('ROLE_USER');
 
-INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES ('admin', '12345', 1, 'Santiago', 'Noguera', 'santi.nog23@gmail.com');
-INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES ('user1', '67890', 1, 'Johela', 'Picón', 'lizecita111@gmail.com');
-INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES ('user2', '09876', 0, 'Omar', 'Noguera', 'omano@hotmail.com');
-INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES ('user3', '54321', 1, 'Sandra', 'Pardo', 'smpardov@gmail.com');
+INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$BV1fW1cNB3occhx/5sKKVuKSaZ3gl/YLD4n2SEuIlY4QWEn8aQntO', 1, 'Santiago', 'Noguera', 'santi.nog23@gmail.com');
+INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES ('user1', '$2a$10$we/0.AO5TYrb1PS7ltEsIev1sVayNxUNVEB...uic9Iv6SakSJ04u', 1, 'Johela', 'Picón', 'lizecita111@gmail.com');
+INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES ('user2', '$2a$10$VrL5BlrCBBd5bbi7KRRJvuUiZYwX07ZMWdAWpLc16BYFP1yGyQKdW', 0, 'Omar', 'Noguera', 'omano@hotmail.com');
+INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES ('user3', '$2a$10$2MauTJiLWptZ/Ho4CdDuGea6PO/XZc.Y4Ns5N9Fwg.xdBHvhmdJxy', 1, 'Sandra', 'Pardo', 'smpardov@gmail.com');
 
 INSERT INTO usuario_roles(usuario_id, role_id) VALUES (1, 1);
 INSERT INTO usuario_roles(usuario_id, role_id) VALUES (1, 2);
